fd076dd32bd5c2c53303396a7f39108d4159e974
Tom Greenwood <tomgreenwood1@gmail.com>
2024-04-04 10:50:20 +0100

    tweak three.js demo

--- demo-03-three-js/index.ts
+++ demo-03-three-js/index.ts
@@ -4,9 +4,18 @@ import {
     AmbientLight, 
     DirectionalLight, 
     WebGLRenderer, 
-    Group} from "three";
+    Mesh,
+    Group,
+    MeshPhongMaterial,
+    Color,
+    Vector3} from "three";
 
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+
+const CAMERA_START = new Vector3(0, 3.5, 10);
+const DIRECTIONAL_LIGHT_MOTION_RANGE = 0.02;
+const DIRECTIONAL_LIGHT_MOTION_FREQUENCY = 1/700;
 
 export async function main(canvas: HTMLCanvasElement) {
 
@@ -17,20 +26,40 @@ export async function main(canvas: HTMLCanvasElement) {
     renderer.setClearColor('white');
     const scene = new Scene();
     const camera = new PerspectiveCamera(60, canvas.clientWidth / canvas.clientHeight);
-    camera.translateZ(10)
-    camera.translateY(3.5)
+   
+
+    const controls = new OrbitControls(camera, canvas);
+
+    camera.position.copy(CAMERA_START);
+    
+    
 
     let model: Group | undefined = undefined;
+    const modelColor = new Color(0.2, 0.9, 1.0);
     
     const objLoader = new OBJLoader();
-    objLoader.load('/rainbowtree.obj', (root) => {
-        console.log(root)
+    objLoader.load('/rainbowtree.obj', (root: Group) => {
+        root.children.forEach((child: Mesh) => {
+        
+            if (Array.isArray(child.material)) {
+                child.material.forEach((material: MeshPhongMaterial) => material.color = modelColor)
+            }  else {
+                // @ts-ignore this is just true for the rainbow model, probably unsafe for others
+                const material: MeshPhongMaterial = child.material; 
+                material.color  = modelColor;
+        }       })
+        
         model = root;
+    
+        model.rotateY(Math.PI/2)
+        controls.target.set(0,CAMERA_START.y, 0)
         scene.add(model)
     });
 
-    const ambientLight = new AmbientLight();
-    const directionalLight = new DirectionalLight();
+    const ambientLight = new AmbientLight(0xffffff, 0.1);
+    const directionalLight = new DirectionalLight(0xffffff, 0.8);
+    directionalLight.translateZ(0.3);
+    
 
     scene.add(ambientLight, directionalLight, camera);
 
@@ -39,7 +68,7 @@ export async function main(canvas: HTMLCanvasElement) {
         if (!oldTimestamp) {
             oldTimestamp = newTimestamp;
         } else {
-            model?.rotateY(0.01)
+            directionalLight.translateX(DIRECTIONAL_LIGHT_MOTION_RANGE*Math.sin(newTimestamp*DIRECTIONAL_LIGHT_MOTION_FREQUENCY)); // move light
             renderer.render(scene, camera);
             oldTimestamp = newTimestamp;
         }
