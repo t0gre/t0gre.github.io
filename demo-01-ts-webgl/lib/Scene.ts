9dd9287e5ee8d81c79ea3dc39a771c9d6998fd7e
Tom Greenwood <tomgreenwood1@gmail.com>
2025-07-16 12:18:31 +0100

    test for parenting

--- demo-01-ts-webgl/lib/Scene.ts
+++ demo-01-ts-webgl/lib/Scene.ts
@@ -9,7 +9,19 @@ export type Pose = {
 export type SceneNode = {
    pose: Pose;
    parent?: SceneNode;
+   children: SceneNode[]; // empty array if no children
    mesh?: Mesh;
+}
+
+export function setParent(node: SceneNode, parent: SceneNode) {
+   
+   // Remove node from its current parent's children array
+   if (node.parent) {
+      node.parent.children = node.parent.children.filter(child => child !== node); // might be slow, but it's simple
+   }
+
+   node.parent = parent;
+   parent.children.push(node);
 }
 
 export type Scene = SceneNode[];
