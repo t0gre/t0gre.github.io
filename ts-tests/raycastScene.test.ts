e585b76a56d419c5b491d55f8287ef866a392b07
Tom Greenwood <tomgreenwood1@gmail.com>
2025-09-25 14:52:49 +0100

    add test for nested node

--- ts-tests/raycastScene.test.ts
+++ ts-tests/raycastScene.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { Ray, rayIntersectsSceneNode } from "demo-01-ts-webgl/lib/raycast";
 import { Vertices } from 'demo-01-ts-webgl/lib/mesh';
-import { initSceneNode } from 'demo-01-ts-webgl/lib/scene';
+import { initSceneNode, setParent } from 'demo-01-ts-webgl/lib/scene';
 import { m4fromPositionAndEuler } from 'demo-01-ts-webgl/lib/mat4';
 
 
@@ -30,16 +30,18 @@ const vertices: Vertices = {
   normals
 } 
         
-const node =  initSceneNode(m4fromPositionAndEuler([-2,0,0], [0,0,0]), {
+
+   
+
+test('it correctly finds an intersection with position transform', () => {
+   
+    const node =  initSceneNode(m4fromPositionAndEuler([-2,0,0], [0,0,0]), {
         vertices,
         material: {
             color: [1,1,1,1]
         }
     })
-   
 
-test('it correctly finds an intersection in the right place', () => {
-   
     const ray: Ray = {
     origin: [-11, 0.5, 0],
     direction: [0, -1, 0]
@@ -52,4 +54,49 @@ test('it correctly finds an intersection in the right place', () => {
 
 })
 
+test('it correctly finds an intersection with multiple position transforms', () => {
+   
+    const node =  initSceneNode(m4fromPositionAndEuler([-2,0,0], [0,0,0]), {
+        vertices,
+        material: {
+            color: [1,1,1,1]
+        }
+    })
 
+    const parentNode =  initSceneNode(m4fromPositionAndEuler([-2,0,0], [0,0,0]))
+
+    setParent(node, parentNode)
+
+    const ray: Ray = {
+    origin: [-11, 0.5, 0],
+    direction: [0, -1, 0]
+    }
+
+    const result = rayIntersectsSceneNode(ray, parentNode)
+
+    const expected = [[-11, 0.0, 0]]
+    expect(result, "intersection is correct").toEqual(expected)
+
+})
+
+
+// test('it correctly finds an intersection with rotation transform', () => {
+   
+//     const node =  initSceneNode(m4fromPositionAndEuler([-2,0,0], [0,0,Math.PI/4]), {
+//         vertices,
+//         material: {
+//             color: [1,1,1,1]
+//         }
+//     })
+
+//     const ray: Ray = {
+//     origin: [-1, 0.5, 0],
+//     direction: [0, -1, 0]
+//     }
+
+//     const result = rayIntersectsSceneNode(ray, node)
+
+//     const expected = [[-1, 0.0, -1]]
+//     expect(result, "intersection is correct").toEqual(expected)
+
+// })
