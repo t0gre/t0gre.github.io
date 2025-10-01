9c4d6756541d9032103fcb7037340c56d8f7211d
Tom Greenwood <tomgreenwood1@gmail.com>
2025-10-01 15:40:19 +0100

    more vectors

--- demo-02-c-opengl/tests/tests.cpp
+++ demo-02-c-opengl/tests/tests.cpp
@@ -2,64 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <vector>
 #include "my_string.h"
 
 #include "test_helpers.cpp" // include before tests
 #include "raycast_triangle_tests.cpp"
 #include "raycast_vertices_tests.cpp"
 
-
-typedef struct {
-    size_t size;
-    size_t capacity;
-    TestResult *array;
-} TestResultArray;
-
-TestResultArray *createTestResultArray(size_t initial_capacity) {
-    TestResultArray *arr = (TestResultArray *)malloc(sizeof(TestResultArray));
-    arr->size = 0;
-    arr->capacity = initial_capacity;
-    arr->array = (TestResult*)malloc(initial_capacity * sizeof(TestResult));
-    return arr;
-}
-
-void addTestResult(TestResultArray *arr, TestResult result) {
-    if (arr->size >= arr->capacity) {
-        arr->capacity *= 2;
-        arr->array = (TestResult*)realloc(arr->array, arr->capacity * sizeof(TestResult));
-    }
-    arr->array[arr->size++] = result;
-}
-
-void freeTestResultArray(TestResultArray *arr) {
-    free(arr->array);
-    free(arr);
-}
-
     
 int main(int argc, char** argv) {
-    TestResultArray *results = createTestResultArray(4);
+    std::vector<TestResult> results;
 
-    
     // triangle tests
-    addTestResult(results, intersect_triangle());
-    addTestResult(results, dont_intersect_because_origin());
-    addTestResult(results, dont_intersect_because_direction());
-
-    // mesh tests
-    addTestResult(results, intersect_vertices_first());
-    addTestResult(results, intersect_vertices_last());
+    results.push_back(intersect_triangle());
+    results.push_back(dont_intersect_because_origin());
+    results.push_back(dont_intersect_because_direction());
+    
+    // mesh.push_back(ests
+    results.push_back(intersect_vertices_first());
+    results.push_back(intersect_vertices_last());
 
     int total = 0;
     int passed = 0;
     int failed = 0;
 
     // Print results
-    for (size_t i = 0; i < results->size; ++i) {
+    for (size_t i = 0; i < results.size(); ++i) {
         total++;
         String * result_message = createString(100);
-        const char * test_message = results->array[i].message;
-        bool test_result = results->array[i].pass;
+        const char * test_message = results.at(i).message;
+        bool test_result = results.at(i).pass;
 
         char test_number_string[50];
         snprintf(test_number_string, sizeof(test_number_string), "test result %d: ", total);
@@ -82,8 +54,6 @@ int main(int argc, char** argv) {
 
     // set the output color to green
     printf("\033[0;32m");
-
-    freeTestResultArray(results);
 
     if (failed) {
 
