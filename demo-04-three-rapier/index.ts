04521f6f80fb37c38553212bebba0d3cecbcb417
Tom Greenwood <tomgreenwood1@gmail.com>
2025-10-09 16:55:23 +0100

    setup rapier

---
+++ demo-04-three-rapier/index.ts
@@ -0,0 +1,85 @@
+import { AmbientLight, Color, DirectionalLight, Fog, Mesh, MeshStandardMaterial, PerspectiveCamera, PlaneGeometry, Scene, Vector3, WebGLRenderer } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+
+const CAMERA_START = new Vector3(0, 3.5, 10);
+const BACKGROUND_COLOR = 0x7799ff
+
+export async function main(canvas: HTMLCanvasElement) {
+
+    const RAPIER = await import('@dimforge/rapier3d')
+
+    let gravity = { x: 0.0, y: -9.81, z: 0.0 };
+    let world = new RAPIER.World(gravity);
+    
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+
+    const renderer = new WebGLRenderer({ canvas, antialias: true });
+    
+    renderer.shadowMap.enabled = true;
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(60, canvas.clientWidth / canvas.clientHeight);
+   
+
+    const controls = new OrbitControls(camera, canvas);
+    controls.maxPolarAngle = Math.PI / 2 - 0.01
+    controls.maxDistance = 13
+    controls.minDistance = 3.5
+    controls.enablePan = false
+
+    camera.position.copy(CAMERA_START);
+ 
+
+   
+    const ambientLight = new AmbientLight(0xffffff, 0.1);
+    const directionalLight = new DirectionalLight(0xffffff, 0.8);
+    directionalLight.translateY(5);
+    directionalLight.castShadow =true;
+    // these values are pure trial an error 
+    directionalLight.shadow.camera.far = 50
+    directionalLight.shadow.camera.bottom = 0
+    directionalLight.shadow.camera.top = 50
+    directionalLight.shadow.camera.near = 0.1
+    directionalLight.shadow.camera.zoom = 1
+    directionalLight.shadow.mapSize.width = 2048
+    directionalLight.shadow.mapSize.height = 2048
+    directionalLight.translateZ(4);
+  
+
+    const floor = new Mesh(new PlaneGeometry(400, 400), new MeshStandardMaterial({color: 0xddcc99}))
+    floor.rotateX(-Math.PI/2)
+    floor.receiveShadow = true;
+
+    scene.background = new Color(BACKGROUND_COLOR);
+    scene.add(ambientLight, directionalLight, camera, floor);
+    scene.fog = new Fog( BACKGROUND_COLOR, 10, 100 );
+
+    let oldTimestamp = 0;
+    function animate(newTimestamp: number): void {
+        if (!oldTimestamp) {
+            oldTimestamp = newTimestamp;
+        } else {
+            
+            const dt = newTimestamp - oldTimestamp
+            
+            
+            renderer.render(scene, camera);
+            oldTimestamp = newTimestamp;
+        }
+       
+        requestAnimationFrame(animate)
+    }
+    
+    requestAnimationFrame(animate)
+    window.addEventListener('resize', () => {
+        const newWidth = canvas.clientWidth;
+        const newHeight = canvas.clientHeight;
+        camera.aspect = newWidth / newHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(newHeight, newHeight, false)
+    })
+
+
+
+}
