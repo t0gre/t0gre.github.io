1c6162564e4596419b0e9830cd4c80f0da65ab48
Tom Greenwood <tomgreenwood1@gmail.com>
2025-10-10 10:58:57 +0100

    upgrade three

--- demo-04-three-jolt/index.ts
+++ demo-04-three-jolt/index.ts
@@ -1,5 +1,25 @@
-import { AmbientLight, Color, DirectionalLight, Fog, Mesh, MeshStandardMaterial, PerspectiveCamera, PlaneGeometry, Scene, Vector3, WebGLRenderer } from "three";
+import  { 
+    AmbientLight, 
+    BoxGeometry, 
+    Color, 
+    DirectionalLight, 
+    Fog, 
+    InstancedMesh, 
+    Matrix4, 
+    Mesh, 
+    MeshBasicMaterial, 
+    MeshLambertMaterial, 
+    MeshStandardMaterial, 
+    PerspectiveCamera, 
+    PlaneGeometry, 
+    Scene, 
+    Vector3, 
+    WebGLRenderer 
+} from "three";
+
+import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { JoltPhysics } from 'three/examples/jsm/physics/JoltPhysics'
 import initJolt from "jolt-physics";
 import joltWasmUrl from "jolt-physics/jolt-physics.wasm.wasm?url";
 
@@ -58,6 +78,53 @@ export async function main(canvas: HTMLCanvasElement) {
     scene.background = new Color(BACKGROUND_COLOR);
     scene.add(ambientLight, directionalLight, camera, floor);
     scene.fog = new Fog( BACKGROUND_COLOR, 10, 100 );
+
+    const floorCollider = new Mesh(
+					new BoxGeometry( 10, 5, 10 ),
+					new MeshBasicMaterial( { color: 0x666666 } )
+				);
+				floorCollider.position.y = - 2.5;
+				floorCollider.userData.physics = { mass: 0 };
+				floorCollider.visible = false;
+				scene.add( floorCollider );
+
+    const matrix = new Matrix4();
+	const color = new Color();
+    const material = new MeshLambertMaterial();
+
+	// Boxes
+
+    const geometryBox = new BoxGeometry( 0.075, 0.075, 0.075 );
+    const boxes = new InstancedMesh( geometryBox, material, 400 );
+    boxes.instanceMatrix.setUsage( THREE.DynamicDrawUsage ); // will be updated every frame
+    boxes.castShadow = true;
+    boxes.receiveShadow = true;
+    boxes.userData.physics = { mass: 1 };
+    scene.add( boxes );
+
+    setInterval( () => {
+
+					let index = Math.floor( Math.random() * boxes.count );
+
+					position.set( 0, Math.random() + 1, 0 );
+					physics.setMeshPosition( boxes, position, index );
+
+					//
+
+					index = Math.floor( Math.random() * spheres.count );
+
+					position.set( 0, Math.random() + 1, 0 );
+					physics.setMeshPosition( spheres, position, index );
+
+				}, 1000 / 60 );
+
+    for ( let i = 0; i < boxes.count; i ++ ) {
+
+        matrix.setPosition( Math.random() - 0.5, Math.random() * 2, Math.random() - 0.5 );
+        boxes.setMatrixAt( i, matrix );
+        boxes.setColorAt( i, color.setHex( 0xffffff * Math.random() ) );
+
+    }
 
     let oldTimestamp = 0;
     function animate(newTimestamp: number): void {
