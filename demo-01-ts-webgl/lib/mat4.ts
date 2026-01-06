f353c71318eaf22bc8e49644cf3c6753de7fa155
Tom Greenwood <tomgreenwood1@gmail.com>
2026-01-06 09:22:34 +0000

    start m4 structure change

--- demo-01-ts-webgl/lib/mat4.ts
+++ demo-01-ts-webgl/lib/mat4.ts
@@ -44,6 +44,47 @@ export type Mat4ZRotation = [
     0,      0,      0,      1,
 ]
 
+type Mat4Positions = {
+    m00: number
+    m01: number
+    m02: number
+    m03: number
+    m10: number
+    m11: number
+    m12: number
+    m13: number
+    m20: number
+    m21: number
+    m22: number
+    m23: number
+    m30: number
+    m31: number
+    m32: number
+    m33: number
+}
+
+function m4AsPositions(m: Mat4): Mat4Positions {
+        return {
+             m00 : m[0 * 4 + 0]!,
+             m01 : m[0 * 4 + 1]!,
+             m02 : m[0 * 4 + 2]!,
+             m03 : m[0 * 4 + 3]!,
+             m10 : m[1 * 4 + 0]!,
+             m11 : m[1 * 4 + 1]!,
+             m12 : m[1 * 4 + 2]!,
+             m13 : m[1 * 4 + 3]!,
+             m20 : m[2 * 4 + 0]!,
+             m21 : m[2 * 4 + 1]!,
+             m22 : m[2 * 4 + 2]!,
+             m23 : m[2 * 4 + 3]!,
+             m30 : m[3 * 4 + 0]!,
+             m31 : m[3 * 4 + 1]!,
+             m32 : m[3 * 4 + 2]!,
+             m33 : m[3 * 4 + 3]!,
+        }  
+}
+
+
 export function m4lookAt(cameraPosition: Vec3, target: Vec3, up: Vec3): Mat4 {
         const zAxis = subtractVectors(cameraPosition, target)
         normalize(zAxis)
@@ -103,55 +144,26 @@ export function m4orthographic(left: number, right: number, bottom: number, top:
 
 export function m4multiply(a: Mat4, b: Mat4): Mat4 {
 
-        const a00 = a[0 * 4 + 0]!;
-        const a01 = a[0 * 4 + 1]!;
-        const a02 = a[0 * 4 + 2]!;
-        const a03 = a[0 * 4 + 3]!;
-        const a10 = a[1 * 4 + 0]!;
-        const a11 = a[1 * 4 + 1]!;
-        const a12 = a[1 * 4 + 2]!;
-        const a13 = a[1 * 4 + 3]!;
-        const a20 = a[2 * 4 + 0]!;
-        const a21 = a[2 * 4 + 1]!;
-        const a22 = a[2 * 4 + 2]!;
-        const a23 = a[2 * 4 + 3]!;
-        const a30 = a[3 * 4 + 0]!;
-        const a31 = a[3 * 4 + 1]!;
-        const a32 = a[3 * 4 + 2]!;
-        const a33 = a[3 * 4 + 3]!;
-        const b00 = b[0 * 4 + 0]!;
-        const b01 = b[0 * 4 + 1]!;
-        const b02 = b[0 * 4 + 2]!;
-        const b03 = b[0 * 4 + 3]!;
-        const b10 = b[1 * 4 + 0]!;
-        const b11 = b[1 * 4 + 1]!;
-        const b12 = b[1 * 4 + 2]!;
-        const b13 = b[1 * 4 + 3]!;
-        const b20 = b[2 * 4 + 0]!;
-        const b21 = b[2 * 4 + 1]!;
-        const b22 = b[2 * 4 + 2]!;
-        const b23 = b[2 * 4 + 3]!;
-        const b30 = b[3 * 4 + 0]!;
-        const b31 = b[3 * 4 + 1]!;
-        const b32 = b[3 * 4 + 2]!;
-        const b33 = b[3 * 4 + 3]!;
+        const ap = m4AsPositions(a);
+        const bp = m4AsPositions(b);
+        
         return [
-            b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
-            b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
-            b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
-            b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
-            b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
-            b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
-            b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
-            b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
-            b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
-            b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
-            b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
-            b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
-            b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
-            b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
-            b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
-            b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+            bp.m00 * ap.m00 + bp.m01 * ap.m10 + bp.m02 * ap.m20 + bp.m03 * ap.m30,
+            bp.m00 * ap.m01 + bp.m01 * ap.m11 + bp.m02 * ap.m21 + bp.m03 * ap.m31,
+            bp.m00 * ap.m02 + bp.m01 * ap.m12 + bp.m02 * ap.m22 + bp.m03 * ap.m32,
+            bp.m00 * ap.m03 + bp.m01 * ap.m13 + bp.m02 * ap.m23 + bp.m03 * ap.m33,
+            bp.m10 * ap.m00 + bp.m11 * ap.m10 + bp.m12 * ap.m20 + bp.m13 * ap.m30,
+            bp.m10 * ap.m01 + bp.m11 * ap.m11 + bp.m12 * ap.m21 + bp.m13 * ap.m31,
+            bp.m10 * ap.m02 + bp.m11 * ap.m12 + bp.m12 * ap.m22 + bp.m13 * ap.m32,
+            bp.m10 * ap.m03 + bp.m11 * ap.m13 + bp.m12 * ap.m23 + bp.m13 * ap.m33,
+            bp.m20 * ap.m00 + bp.m21 * ap.m10 + bp.m22 * ap.m20 + bp.m23 * ap.m30,
+            bp.m20 * ap.m01 + bp.m21 * ap.m11 + bp.m22 * ap.m21 + bp.m23 * ap.m31,
+            bp.m20 * ap.m02 + bp.m21 * ap.m12 + bp.m22 * ap.m22 + bp.m23 * ap.m32,
+            bp.m20 * ap.m03 + bp.m21 * ap.m13 + bp.m22 * ap.m23 + bp.m23 * ap.m33,
+            bp.m30 * ap.m00 + bp.m31 * ap.m10 + bp.m32 * ap.m20 + bp.m33 * ap.m30,
+            bp.m30 * ap.m01 + bp.m31 * ap.m11 + bp.m32 * ap.m21 + bp.m33 * ap.m31,
+            bp.m30 * ap.m02 + bp.m31 * ap.m12 + bp.m32 * ap.m22 + bp.m33 * ap.m32,
+            bp.m30 * ap.m03 + bp.m31 * ap.m13 + bp.m32 * ap.m23 + bp.m33 * ap.m33,
         ];
     }
 
@@ -265,87 +277,73 @@ export function m4transpose(m: Mat4) {
   }
 
 export function m4inverse(m: Mat4): Mat4 {
-        const m00 = m[0 * 4 + 0]!;
-        const m01 = m[0 * 4 + 1]!;
-        const m02 = m[0 * 4 + 2]!;
-        const m03 = m[0 * 4 + 3]!;
-        const m10 = m[1 * 4 + 0]!;
-        const m11 = m[1 * 4 + 1]!;
-        const m12 = m[1 * 4 + 2]!;
-        const m13 = m[1 * 4 + 3]!;
-        const m20 = m[2 * 4 + 0]!;
-        const m21 = m[2 * 4 + 1]!;
-        const m22 = m[2 * 4 + 2]!;
-        const m23 = m[2 * 4 + 3]!;
-        const m30 = m[3 * 4 + 0]!;
-        const m31 = m[3 * 4 + 1]!;
-        const m32 = m[3 * 4 + 2]!;
-        const m33 = m[3 * 4 + 3]!;
-        const tmp_0 = m22 * m33;
-        const tmp_1 = m32 * m23;
-        const tmp_2 = m12 * m33;
-        const tmp_3 = m32 * m13;
-        const tmp_4 = m12 * m23;
-        const tmp_5 = m22 * m13;
-        const tmp_6 = m02 * m33;
-        const tmp_7 = m32 * m03;
-        const tmp_8 = m02 * m23;
-        const tmp_9 = m22 * m03;
-        const tmp_10 = m02 * m13;
-        const tmp_11 = m12 * m03;
-        const tmp_12 = m20 * m31;
-        const tmp_13 = m30 * m21;
-        const tmp_14 = m10 * m31;
-        const tmp_15 = m30 * m11;
-        const tmp_16 = m10 * m21;
-        const tmp_17 = m20 * m11;
-        const tmp_18 = m00 * m31;
-        const tmp_19 = m30 * m01;
-        const tmp_20 = m00 * m21;
-        const tmp_21 = m20 * m01;
-        const tmp_22 = m00 * m11;
-        const tmp_23 = m10 * m01;
+        const p = m4AsPositions(m)
 
-        const t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
-            (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
-        const t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
-            (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
-        const t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
-            (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
-        const t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
-            (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+        const tmp_0 =  p.m22 * p.m33;
+        const tmp_1 =  p.m32 * p.m23;
+        const tmp_2 =  p.m12 * p.m33;
+        const tmp_3 =  p.m32 * p.m13;
+        const tmp_4 =  p.m12 * p.m23;
+        const tmp_5 =  p.m22 * p.m13;
+        const tmp_6 =  p.m02 * p.m33;
+        const tmp_7 =  p.m32 * p.m03;
+        const tmp_8 =  p.m02 * p.m23;
+        const tmp_9 =  p.m22 * p.m03;
+        const tmp_10 = p.m02 * p.m13;
+        const tmp_11 = p.m12 * p.m03;
+        const tmp_12 = p.m20 * p.m31;
+        const tmp_13 = p.m30 * p.m21;
+        const tmp_14 = p.m10 * p.m31;
+        const tmp_15 = p.m30 * p.m11;
+        const tmp_16 = p.m10 * p.m21;
+        const tmp_17 = p.m20 * p.m11;
+        const tmp_18 = p.m00 * p.m31;
+        const tmp_19 = p.m30 * p.m01;
+        const tmp_20 = p.m00 * p.m21;
+        const tmp_21 = p.m20 * p.m01;
+        const tmp_22 = p.m00 * p.m11;
+        const tmp_23 = p.m10 * p.m01;
 
-        const d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+        const t0 = (tmp_0 * p.m11 + tmp_3 * p.m21 + tmp_4 * p.m31) -
+            (tmp_1 * p.m11 + tmp_2 * p.m21 + tmp_5 * p.m31);
+        const t1 = (tmp_1 * p.m01 + tmp_6 * p.m21 + tmp_9 * p.m31) -
+            (tmp_0 * p.m01 + tmp_7 * p.m21 + tmp_8 * p.m31);
+        const t2 = (tmp_2 * p.m01 + tmp_7 * p.m11 + tmp_10 * p.m31) -
+            (tmp_3 * p.m01 + tmp_6 * p.m11 + tmp_11 * p.m31);
+        const t3 = (tmp_5 * p.m01 + tmp_8 * p.m11 + tmp_11 * p.m21) -
+            (tmp_4 * p.m01 + tmp_9 * p.m11 + tmp_10 * p.m21);
+
+        const d = 1.0 / (p.m00 * t0 + p.m10 * t1 + p.m20 * t2 + p.m30 * t3);
 
         return [
             d * t0,
             d * t1,
             d * t2,
             d * t3,
-            d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
-                (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
-            d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
-                (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
-            d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
-                (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
-            d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
-                (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
-            d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
-                (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
-            d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
-                (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
-            d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
-                (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
-            d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
-                (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
-            d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
-                (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
-            d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
-                (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
-            d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
-                (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
-            d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
-                (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
+            d * ((tmp_1 *  p.m10 + tmp_2 *  p.m20 + tmp_5 *  p.m30) -
+                (tmp_0 *   p.m10 + tmp_3 *  p.m20 + tmp_4 *  p.m30)),
+            d * ((tmp_0 *  p.m00 + tmp_7 *  p.m20 + tmp_8 *  p.m30) -
+                (tmp_1 *   p.m00 + tmp_6 *  p.m20 + tmp_9 *  p.m30)),
+            d * ((tmp_3 *  p.m00 + tmp_6 *  p.m10 + tmp_11 * p.m30) -
+                (tmp_2 *   p.m00 + tmp_7 *  p.m10 + tmp_10 * p.m30)),
+            d * ((tmp_4 *  p.m00 + tmp_9 *  p.m10 + tmp_10 * p.m20) -
+                (tmp_5 *   p.m00 + tmp_8 *  p.m10 + tmp_11 * p.m20)),
+            d * ((tmp_12 * p.m13 + tmp_15 * p.m23 + tmp_16 * p.m33) -
+                (tmp_13 *  p.m13 + tmp_14 * p.m23 + tmp_17 * p.m33)),
+            d * ((tmp_13 * p.m03 + tmp_18 * p.m23 + tmp_21 * p.m33) -
+                (tmp_12 *  p.m03 + tmp_19 * p.m23 + tmp_20 * p.m33)),
+            d * ((tmp_14 * p.m03 + tmp_19 * p.m13 + tmp_22 * p.m33) -
+                (tmp_15 *  p.m03 + tmp_18 * p.m13 + tmp_23 * p.m33)),
+            d * ((tmp_17 * p.m03 + tmp_20 * p.m13 + tmp_23 * p.m23) -
+                (tmp_16 *  p.m03 + tmp_21 * p.m13 + tmp_22 * p.m23)),
+            d * ((tmp_14 * p.m22 + tmp_17 * p.m32 + tmp_13 * p.m12) -
+                (tmp_16 *  p.m32 + tmp_12 * p.m12 + tmp_15 * p.m22)),
+            d * ((tmp_20 * p.m32 + tmp_12 * p.m02 + tmp_19 * p.m22) -
+                (tmp_18 *  p.m22 + tmp_21 * p.m32 + tmp_13 * p.m02)),
+            d * ((tmp_18 * p.m12 + tmp_23 * p.m32 + tmp_15 * p.m02) -
+                (tmp_22 *  p.m32 + tmp_14 * p.m02 + tmp_19 * p.m12)),
+            d * ((tmp_22 * p.m22 + tmp_16 * p.m02 + tmp_21 * p.m12) -
+                (tmp_20 *  p.m12 + tmp_23 * p.m22 + tmp_17 * p.m02))
         ];
     }
 
